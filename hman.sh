8ae46afa8783cba52480173fdf0784b6cb14bb80
AoHRuthless <kumara0398@gmail.com>
2017-12-15 18:12:24 -0500

    add dictionary to shell script

--- hman.sh
+++ hman.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-# Compiles and runs Hangman. Since this is a basic project, no extra args
-# or features are supported.
-py hangman.py
+# Compiles and runs Hangman using the provided word dictionary.
+py hangman.py dictionary.txt
